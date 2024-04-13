b2139fa4003f451cdae440dc23e3acec90a9836a
Adam Tilkens <adamtilkens@arizona.edu>
2024-04-12 17:13:21 -0700

    Added a comprehensive test case which utilizes words.txt

--- src/test/TestTries.java
+++ src/test/TestTries.java
@@ -2,6 +2,10 @@ package test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Collections;
@@ -54,7 +58,6 @@ class TestTries {
 		// Add @maxElements random strings
 		for (int i = 0; i < maxElements; i++) {
 			String string = generateRandomString(i, alpha1, 1, 50);
-			System.out.println(string);
 
 			trie.insert(string);
 			randomStrings.add(string);
@@ -67,13 +70,94 @@ class TestTries {
 		// Remove the added strings
 		for (int i = 0; i < maxElements; i++) {
 			String string = randomStrings.get(i);
-			System.out.println(string);
 
-//			assertTrue(trie.search(string));
+
+			assertTrue(trie.search(string));
 			trie.search(string);
 			trie.delete(string);
 			assertFalse(trie.search(string));
 		}
+	}
+	
+	@Test
+	void testWordList() {
+		Trie trie = new Trie();
+		String words[] = new String[100];
+		int trieTotalWords = 0;
+		int numWords = 0;
+		Random rng = new Random();
+		boolean test = false;
+		
+		try {
+			// Read words.txt and add all words to Trie
+			BufferedReader reader = new BufferedReader(new FileReader("words.txt"));
+			String line = reader.readLine();
+			
+			// Fill list with ~300k words
+			System.out.println("Filling Trie...");
+			while (line != null) {
+				trie.insert(line);	
+				trieTotalWords++;
+				// 1/3000 chance to add to word list to check against later, max 100.
+				if(rng.nextInt(3000) == 1) {
+					if (numWords < 100) {
+						words[numWords] = line;
+						numWords++;
+					}					
+				}
+				
+				line = reader.readLine();
+			}		
+			reader.close();
+			System.out.println("Added " + trieTotalWords + " words to the Trie!");
+			
+			
+			System.out.println("-------------------------------");
+			System.out.println("Searching for words...");
+			System.out.println("-------------------------------");			
+			
+			for (int i = 0; i < numWords; i++) {
+				
+				test = trie.search(words[i]);
+				assertTrue(test);
+				if (test) {
+					System.out.println("Successfully found word: " + words[i]);
+				} else {
+					System.out.println("Expected, but did not find: " + words[i]);
+				}
+			}
+			
+			System.out.println("-------------------------------");
+			System.out.println("Removing words...");
+			System.out.println("-------------------------------");		
+			
+			int index = rng.nextInt(numWords - 8);
+			for (int j = 0; j < 7; j++) {
+				String check = words[index + j];
+				System.out.println("Removing: " + check + "...");
+				trie.delete(check);			
+				
+				test = !(trie.search(check));
+				assertTrue(test);
+				if (test) {
+					System.out.println("Succesfully Removed " + check + "!\n");
+				} else {
+					System.out.println(check + " was not removed.");
+				}
+			}
+			
+			
+		} catch (FileNotFoundException e) {
+			System.out.println("Source Words File Not Found");
+			fail();
+		} catch (IOException e) {
+			fail();
+			e.printStackTrace();			
+		}
+		
+		
+		
+		
 	}
 
 	private String generateRandomString(int seed, String alphabet, int minLength, int maxLength) {
