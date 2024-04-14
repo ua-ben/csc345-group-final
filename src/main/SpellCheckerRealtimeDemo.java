8ef448cf90d31159985df54b7d7a071a327d97f9
ua-ben <benseckeler@arizona.edu>
2024-04-13 17:43:32 -0700

    add SpellCheckerRealtimeDemo

---
+++ src/main/SpellCheckerRealtimeDemo.java
@@ -0,0 +1,35 @@
+package main;
+
+import java.io.File;
+import java.util.List;
+import java.util.Scanner;
+
+public class SpellCheckerRealtimeDemo {
+
+	protected static SpellChecker spellChecker;
+
+	public static void main(String[] args) {
+		spellChecker = new SpellChecker("words.txt");
+		Scanner userInputScanner = new Scanner(System.in);
+
+		while (userInputScanner.hasNextLine()) {
+			checkWord(userInputScanner.nextLine());
+		}
+	}
+
+	static void checkWord(String word) {
+		boolean isSpelledCorrectly = !spellChecker.isWordMispelled(word);
+
+		if (isSpelledCorrectly) {
+			return;
+		}
+
+		List<String> suggestions = spellChecker.getSpellingSuggestions(word);
+
+		System.out.println("Did you mean: ");
+		for (int i = 0; i < suggestions.size(); i++) {
+			System.out.println("\t" + suggestions.get(i));
+		}
+	}
+
+}
